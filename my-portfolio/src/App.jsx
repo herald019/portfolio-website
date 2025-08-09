c8db5d9fa631e0a55b4bb31e5d71e7eaea774252
Harry Herald R <harryherald019@gmail.com>
2025-08-09 17:46:00 +0530

    add tailwind

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,14 +6,22 @@ import Contact from './components/contacts';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-green-500">
+        Tailwind is working!
+      </h1>
+      <h1 className="text-green-500 !important">
+  Forced Green Text
+</h1>
+      <h1 className="text-[#10B981]">Direct Green Color</h1>
+
+      <p className="text-gray-600 mt-4">
+        You're ready to build your portfolio ✨
+      </p>
     </div>
   );
 }
+
+
 
 export default App;
